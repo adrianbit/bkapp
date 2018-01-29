36b1e05ceb8767e2184706876645049c21bcc72c
Bitechnology Mx 2 <bitechnology.mx2@gmail.com>
2018-01-29 03:49:35 +0000

    ......

---
+++ APP_BANCARIA_DEV/components/sucursalesView/index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+app.sucursalesView = kendo.observable({
+    onShow: function() {},
+    afterShow: function() {}
+});
+app.localization.registerView('sucursalesView');
+
+// START_CUSTOM_CODE_sucursalesView
+// Add custom code here. For more information about custom code, see http://docs.telerik.com/platform/screenbuilder/troubleshooting/how-to-keep-custom-code-changes
+
+// END_CUSTOM_CODE_sucursalesView
